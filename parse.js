7e33d4bb4581ef868bf6571c15d9dfc90bf8e1df
Craig <cav9871@saintjoe.edu>
2015-03-23 12:14:03 -0400

    This is my INI parse file

---
+++ parse.js
@@ -0,0 +1,36 @@
+﻿//Craig Austgen
+//code from chapter 9 regular expressions
+
+
+function parseINI(string) {
+ // Start with an object to hold the top-level fields
+ var currentSection = {name: null, fields: []};
+ var categories = [currentSection];
+
+ string.split(/\r?\n/).forEach(function(line) {
+   var match;
+   if (/^\s*(;.*)?$/.test(line)) {
+     return;
+   } else if (match = line.match(/^\[(.*)\]$/)) {
+     currentSection = {name: match[1], fields: []};
+     categories.push(currentSection);
+   } else if (match = line.match(/^(\w+)=(.*)$/)) {
+     currentSection.fields.push({name: match[1],
+                                 value: match[2]});
+   } else {
+     throw new Error("Line '" + line + "' is invalid.");
+   }
+ });
+
+ return categories;
+}
+
+print(parseINI([larry]
+			fullname=Larry Doe
+			type=kindergarten bully
+			website=http:
+			
+			[gargamel]
+			fullname=Gargamel
+			type=evil sorcerer
+			outputdir=/home/marijn/enemies/gargamel));
